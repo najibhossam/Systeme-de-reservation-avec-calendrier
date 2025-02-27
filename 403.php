bfc09184f968b7ce3137479e167fc76f145a744c
najibhossam <najibhossam01@gmail.com>
2025-02-27 22:55:44 +0100

    Ajout des fichiers 404, 403
    Ajout des fichiers de gestion de page d'erreur avec htaccess pour la redirection

---
+++ 403.php
@@ -0,0 +1,14 @@
+<?php
+http_response_code(403);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Accès interdit</title>
+</head>
+<body>
+    <h1>Erreur 403</h1>
+    <p>Vous n'êtes pas autorisé à accéder à cette ressource.</p>
+    <a href="index.php">Retour à l'accueil</a>
+</body>
+</html>
---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+http_response_code(404);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page non trouvée</title>
+</head>
+<body>
+    <h1>Erreur 404</h1>
+    <p>Désolé, la page demandée n'existe pas.</p>
+    <a href="index.php">Retour à l'accueil</a>
+</body>
+</html>
