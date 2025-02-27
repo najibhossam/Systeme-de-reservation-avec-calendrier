947a028ea670692863f7d2a742b341df34088a5e
najibhossam <najibhossam01@gmail.com>
2025-02-27 22:04:22 +0100

    Creation du footer.php
    Ajout du footer pour en servir dans tout les pages

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+//
+?>
+</div> 
+
+<div class="footer-container py-3">
+    © <?php echo date('Y'); ?> - Réservation en ligne
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
